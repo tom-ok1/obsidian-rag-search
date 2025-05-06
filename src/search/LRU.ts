cdb5fdbadbee204cb3e779cb8ae1f9371c120588
tom-ok1 <tom.ok1@icloud.com>
2025-05-06 12:49:40 +0900

    feat: implement LRU cache class with basic operations

---
+++ src/search/LRU.ts
@@ -0,0 +1,39 @@
+export class LRU<K, V> {
+	constructor(private readonly max: number) {}
+	private map = new Map<K, V>();
+
+	get(key: K) {
+		const item = this.map.get(key);
+		if (item === undefined) return undefined;
+
+		this.map.delete(key);
+		this.map.set(key, item);
+		return item;
+	}
+
+	/**
+	 * @param onEvict - Callback function to be called when an item is evicted
+	 */
+	set<T>(key: K, val: V, onEvict: (k: K, v: V) => T) {
+		if (this.map.has(key)) this.map.delete(key);
+		this.map.set(key, val);
+		if (this.map.size > this.max) {
+			const oldestKey = this.map.keys().next().value as K;
+			const oldestVal = this.map.get(oldestKey)!;
+			this.map.delete(oldestKey);
+			return onEvict(oldestKey, oldestVal);
+		}
+	}
+
+	has(key: K) {
+		return this.map.has(key);
+	}
+
+	values() {
+		return this.map.values();
+	}
+
+	get length() {
+		return this.map.size;
+	}
+}
